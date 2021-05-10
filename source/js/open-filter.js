b308b24373e24f80ce2a35fd9a6e420926ed8413
arahimov <artemiirakhimov@yandex.ru>
2021-05-10 14:31:29 +0300

    Добавляет финальную версию проекта

--- source/js/open-filter.js
+++ source/js/open-filter.js
@@ -2,12 +2,14 @@ const filterButton = document.querySelector('.filter__button');
 const hideButton = document.querySelector('.filter__hide-button');
 const filterHeader = document.querySelector('.filter__header');
 const filterIndex = document.querySelector('.filter__index');
+const countryGroup = document.querySelector('.group-countries');
 
 filterButton.addEventListener('click', (event) => {
   filterHeader.classList.toggle('filter__header--open');
   event.target.classList.toggle('filter__button--show');
   event.target.classList.toggle('filter__button--hide');
   filterIndex.classList.toggle('filter__index--open');
+  countryGroup.classList.toggle("group-countries--popup");
 })
 
 hideButton.addEventListener('click', () => {
@@ -15,4 +17,5 @@ hideButton.addEventListener('click', () => {
   filterIndex.classList.remove('filter__index--open');
   filterButton.classList.add('filter__button--show');
   filterButton.classList.remove('filter__button--hide');
+  countryGroup.classList.remove("group-countries--popup");
 })
